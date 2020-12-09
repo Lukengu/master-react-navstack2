06a7543454e8415c21863cdd9d180726d993a877
Windows PC <phil_bona@yahoo.fr>
2020-12-09 23:12:36 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import {LoginPage} from "./LoginPage";
+import {HomePage} from "./HomePage";
+import {RegisterPage} from "./RegisterPage";
+
+
+
+
+
+const RootStack = createStackNavigator();
+
+export default class App extends React.Component {
+  render(){
+    return (
+        <NavigationContainer>
+            <RootStack.Navigator>
+              <RootStack.Screen name="LoginPage" component={LoginPage} />
+              <RootStack.Screen name="HomePage" component={HomePage} />
+              <RootStack.Screen name="RegisterPage" component={RegisterPage} />
+            </RootStack.Navigator>
+        </NavigationContainer>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
